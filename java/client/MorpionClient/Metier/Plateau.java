209b8a88852d0823387cbd34101d201820146f99
Empereur-Hanibal <78930962+Empereur-Hanibal@users.noreply.github.com>
2022-05-18 17:27:24 +0200

    jeu morpion, affichage cmd grille

--- java/client/MorpionClient/Metier/Plateau.java
+++ java/client/MorpionClient/Metier/Plateau.java
@@ -14,11 +14,34 @@ public class Plateau
 
         for (int lig=0; lig<grille.length; lig++)
             for (int col=0; col< grille[lig].length; col++)
-            {
-                grille[lig][col] = 'X';
-                System.out.println(grille[lig][col]);
-            }
+                grille[lig][col] = ' ';
+
+        System.out.println(Plateau.afficherGrille (grille));+
     }
 
+    public static String afficherGrille(char[][] tab)
+    {
+        String sRet;
+        String sSepHori;
+
+        sSepHori = "  +";
+        for (int cpt=0; cpt< tab[0].length;cpt++) sSepHori += "---+";
+
+        sRet = "    ";
+        for (int cpt=0; cpt< tab[0].length;cpt++) sRet += (char) ('A' + cpt) + "   ";
+
+        sRet += "\n" + sSepHori + "\n";
+
+        for (int lig=0; lig< tab.length;lig++)
+        {
+            sRet += (lig+1) + " |";
+            for (int col=0; col< tab[lig].length;col++)
+                sRet += " " + tab[lig][col] + " |";
+
+            sRet += "\n" + sSepHori + "\n";
+        }
+
+        return sRet;
+    }
     public static void main(String[] args) {new Plateau();}
 }
