669b3621c8b2c176e4a0abc6bd8b5ddfc4478ae5
AdrienRov <54404616+AdrienRov@users.noreply.github.com>
2022-05-19 11:19:48 +0200

    Correction des fichiers de connexion à la base de donnée

---
+++ java/server/Sortie.java
@@ -0,0 +1,12 @@
+
+public class Sortie 
+{
+   
+    public static void main(String[] args)
+    {
+        ConnectMySQL conn = new ConnectMySQL();
+        
+        System.out.println("TEST SERVER");
+        ConnectMySQL.getTable();
+    }
+}
